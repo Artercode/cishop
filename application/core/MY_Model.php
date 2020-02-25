b70e1afe8ec6e2ef9c896fa8cf7c2540445ea4ca
artercode <arter@gmail.com>
2020-02-25 17:44:43 +0700

    07. Membuat Fungsi Pagination

--- application/core/MY_Model.php
+++ application/core/MY_Model.php
@@ -4,7 +4,9 @@ defined('BASEPATH') or exit('No direct script access allowed');
 class MY_Model extends CI_Model
 {
 
-    protected $table = '';
+    protected $table    = '';
+    // untuk menentukan jumlah data dalam 1 halaman di pagination
+    protected $perPage  = 5;
 
     public function __construct()
     {
@@ -86,6 +88,7 @@ class MY_Model extends CI_Model
         return $this->db->count_all_results($this->table);
     }
 
+
     // CRUD #################################################################################
     // menyimpan data baru ke database
     public function create($data)
@@ -103,6 +106,67 @@ class MY_Model extends CI_Model
     {
         $this->db->delete($this->table);
         return $this->db->affected_rows();
+    }
+
+
+    // pagination ###########################################################################
+    // menampilkan data sesuai perPage di halaman pertama dan seterusnya...
+    // setting perPage ada di atas bagian construct
+    public function paginate($page)
+    {
+        $this->db->limit(
+            $this->perPage,
+            $this->calculateRealOffset($page)
+        );
+        return $this;
+    }
+    // menentukan offset jumlah halaman di pagination
+    public function calculateRealOffset($page)
+    {
+        // jika halaman kosong nilai offset = 0
+        if (is_null($page) || empty($page)) {
+            $offset = 0;
+        } else {
+            // jika ada data / nilai tidak kosong ...
+            $offset = ($page * $this->perPage) - $this->perPage;
+        }
+        return $offset;
+    }
+    // style pagination + rules dan kondisi
+    public function makePagination($baseUrl, $uriSegment, $totalRows = null)
+    {
+        $this->load->library('pagination');
+
+        $config = [
+            'base_url'          => $baseUrl,
+            'uri_segment'       => $uriSegment,
+            'per_page'          => $this->perPage,
+            'total_rows'        => $totalRows,
+            'use_page_numbers'  => true,
+            // desain pagination
+            'full_tag_open'     => '<ul class="pagination">',
+            'full_tag_close'    => '</ul>',
+            'attributes'        => ['class' => 'page-link'],
+            'first_link'        => false,
+            'last_link'         => false,
+            'first_tag_open'    => '<li class="page-item">',
+            'first_tag_close'   => '</li>',
+            'prev_link'         => '&laquo',
+            'prev_tag_open'     => '<li class="page-item">',
+            'prev_tag_close'    => '</li>',
+            'next_link'         => '&raquo',
+            'next_tag_open'     => '<li class="page-item">',
+            'next_tag_close'    => '</li>',
+            'last_tag_open'     => '<li class="page-item">',
+            'last_tag_close'    => '</li>',
+            'cur_tag_open'      => '<li class="page-item active"><a href="#" class="page-link">',
+            'cur_tag_close'     => '<span class="sr-only">(current)</span></a></li>',
+            'num_tag_open'      => '<li class="page-item">',
+            'num_tag_close'     => '</li>',
+        ];
+
+        $this->pagination->initialize($config);
+        return $this->pagination->create_links();
     }
 }
 
