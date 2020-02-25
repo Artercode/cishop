eb47601ddfc0d9ae84af8fc746358bc3fb833a9e
artercode <arter@gmail.com>
2020-02-25 17:29:22 +0700

    06. Membuat Fungsi Umum dari Query Database

--- application/core/MY_Model.php
+++ application/core/MY_Model.php
@@ -32,6 +32,78 @@ class MY_Model extends CI_Model
 
         return $this->form_validation->run();
     }
+
+
+    // seleksi data perkolom
+    public function select($columns)
+    {
+        $this->db->select($columns);
+        return $this;
+    }
+    // mencari data dan harus sama persis
+    public function where($column, $condition)
+    {
+        $this->db->where($column, $condition);
+        return $this;
+    }
+    //  mencari data yang mirip pada suatu kolom
+    public function like($column, $condition)
+    {
+        $this->db->like($column, $condition);
+        return $this;
+    }
+    //  mencari data yang mirip pada semua kolom dalam suatu tabel
+    public function orlike($column, $condition)
+    {
+        $this->db->or_like($column, $condition);
+        return $this;
+    }
+    // mengabungkan dua tabel dengan data sama dalam kolom
+    public function join($table, $type = 'left')
+    {
+        $this->db->join($table, "$this->table.id_$table = $table.id", $type);
+        return $this;
+    }
+    // mengurutkan data tabel secara asc ()
+    public function orderBy($column, $order = 'asc')
+    {
+        $this->db->order_by($column, $order);
+        return $this;
+    }
+    // menampilkan 1 data dari hasil query dan kondisi dalam bentuk object
+    public function first()
+    {
+        return $this->db->get($this->table)->row();
+    }
+    // menampilkan semua data dalam suatu tabel dalam bentuk object
+    public function get()
+    {
+        return $this->db->get($this->table)->result();
+    }
+    // menampilkan nilai jumlah data dalam 1 tabel dari hasil query dan kondisi
+    public function count()
+    {
+        return $this->db->count_all_results($this->table);
+    }
+
+    // CRUD #################################################################################
+    // menyimpan data baru ke database
+    public function create($data)
+    {
+        $this->db->insert($this->table, $data);
+        return $this->db->insert_id();
+    }
+    // edit atau mengubah data di database
+    public function update($data)
+    {
+        return $this->db->update($this->table, $data);
+    }
+    // menghapus data di database
+    public function delete()
+    {
+        $this->db->delete($this->table);
+        return $this->db->affected_rows();
+    }
 }
 
 /* End of file MY_Model.php */
