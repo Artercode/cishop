06d71b47fac8446c045fad804440935fa877dc37
artercode <arter@gmail.com>
2020-03-03 16:39:14 +0700

    63. Menampilkan Data Detail Order

--- application/models/Order_model.php
+++ application/models/Order_model.php
@@ -4,8 +4,8 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class Order_model extends MY_Model
 {
-
-   protected $table = 'orders';
+   // supaya bisa diubah tabel jadi mengunakan public
+   public $table = 'orders';
 }
 
 /* End of file Order_model.php */
--- application/views/pages/order/index.php
+++ application/views/pages/order/index.php
@@ -37,7 +37,7 @@
                   <tbody>
                      <?php foreach ($content as $row) : ?>
                         <tr>
-                           <td><a href=""><strong>#<?= $row->invoice ?></strong></a></td>
+                           <td><a href="<?= base_url("/order/detail/$row->id") ?>"><strong>#<?= $row->invoice ?></strong></a></td>
                            <td><?= str_replace('-', '/', date("d-m-Y", strtotime($row->date))) ?></td>
                            <td>Rp<?= number_format($row->total, 0, ',', '.') ?>,-</td>
                            <td>
